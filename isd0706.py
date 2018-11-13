78371f529d9f677012fe572de0ce2e42d80c06a2
DJRockyFresh <43992131+DJRockyFresh@users.noreply.github.com>
2018-11-13 14:51:42 +0000

    Update isd0706.py

--- isd0706.py
+++ isd0706.py
@@ -1,6 +1,22 @@
 s1 = input("Enter a short sentence: ")
 s2 = input("Enter another short sentence: ")
-s = []
-s.append(s1 + s2)
-s.count(s)
+
+print(s1 + s2)
+s = [s1, s2]
+s = [word for line in s for word in line.split()]
 print(s)
+
+s.sort() 
+print("Sorted:", s)
+
+slength = len(s)
+print("Word count:", slength)
+
+#d = {"s": "sentences"}
+
+
+d = {}
+for i in range (len(s)):
+    d[i] = s[i]
+
+print(d)    
